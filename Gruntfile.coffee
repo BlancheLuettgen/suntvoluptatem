109557dca6424b11523b1e6b7049bd1b3c9869a3
BlancheLuettgen <annetteosbornee815e271620@outlook.com>
2024-03-23 20:05:49 +0800

    facere

---
+++ Gruntfile.coffee
@@ -0,0 +1,156 @@
+require "colors"
+MochaCloud = require "mocha-cloud"
+
+module.exports = (grunt) ->
+  # Configuration
+  grunt.initConfig
+    # Package information
+    pkg: grunt.file.readJSON "package.json"
+
+    # JSHint (see http://www.jshint.com/docs/)
+    jshint:
+      options:
+        # Predefined globals
+        browser: true
+
+        # The Good Parts
+        eqeqeq: true
+        eqnull: true
+        curly: true
+        latedef: true
+        undef: true
+        forin: true
+
+        # Style preferences
+        indent: 2
+        camelcase: true
+        trailing: true
+        quotmark: "double"
+        newcap: true
+
+      dist:
+        files:
+          src: ["src/**/*.js"]
+
+    # File concatenation, copying and templating
+    concat:
+      options:
+        process: true
+      dist:
+        src: ["src/bugsnag.js"]
+        dest: "dist/bugsnag.js"
+
+    # Minification
+    uglify:
+      dist:
+        files:
+          "dist/bugsnag.min.js": ["dist/bugsnag.js"]
+
+    # Upload to s3
+    s3:
+      bucket: "bugsnagcdn"
+      access: "public-read"
+      gzip: true
+
+      upload: [
+        src: "dist/bugsnag.js",
+        dest: "bugsnag-<%= pkg.version %>.js"
+      ,
+        src: "dist/bugsnag.min.js",
+        dest: "bugsnag-<%= pkg.version %>.min.js"
+      ]
+
+    # Version bumping
+    bump:
+      options: part: "patch"
+      files: ["package.json", "component.json"]
+
+    # Web server
+    connect:
+      server:
+        options:
+          hostname: null
+          port: 8888
+
+    # Tests
+    mochaCloud:
+      username: process.env.SAUCE_USERNAME
+      accessKey: process.env.SAUCE_ACCESS_KEY
+      url: "http://localhost:8888/test/"
+      browsers: [
+        ["chrome", "", "Mac 10.8"]
+        ["chrome", "", "Windows 2003"]
+        ["firefox", "11", "Mac 10.6"]
+        ["firefox", "11", "Windows 2003"]
+        ["safari", "5", "Mac 10.6"]
+        ["safari", "6", "Mac 10.8"]
+        ["iexplore", "6", "Windows 2003"]
+        ["iexplore", "7", "Windows 2003"]
+        ["iexplore", "8", "Windows 2003"]
+        ["iexplore", "9", "Windows 2008"]
+      ]
+
+    # Documentation
+    docco:
+      dist:
+        src: ["src/**/*.js"]
+        dest: "docs/"
+
+  # Load tasks from plugins
+  grunt.loadNpmTasks "grunt-contrib-jshint"
+  grunt.loadNpmTasks "grunt-contrib-concat"
+  grunt.loadNpmTasks "grunt-contrib-uglify"
+  grunt.loadNpmTasks "grunt-contrib-connect"
+  grunt.loadNpmTasks "grunt-bumpx"
+  grunt.loadNpmTasks "grunt-s3"
+  grunt.loadNpmTasks "grunt-docco"
+
+  # Task to tag a version in git
+  grunt.registerTask "git-tag", "Tags a release in git", ->
+    exec = require("child_process").exec
+    done = this.async()
+    releaseVersion = grunt.template.process("<%= pkg.version %>")
+
+    child = exec "git ci -am \"v#{releaseVersion}\" && git tag v#{releaseVersion}", (error, stdout, stderr) ->
+      console.log("Error running git tag: " + error) if error?
+      done(!error?)
+
+  # Testing
+  grunt.registerTask "mocha-cloud", "Run mocha browser tests using mocha-cloud and Sauce Labs", ->
+    done = this.async()
+    options = grunt.config.get("mochaCloud")
+
+    # Set up mocha-cloud
+    cloud = new MochaCloud("", options.username, options.accessKey)
+    cloud.url options.url
+    cloud.browser b... for b in options.browsers
+
+    # Progress hooks
+    cloud.on "start", (browser) ->
+      console.log "[#{browser.browserName} #{browser.version} #{browser.platform}] Starting tests"
+    
+    cloud.on "end", (browser, res) ->
+      if res.failures > 0
+        console.log "[#{browser.browserName} #{browser.version} #{browser.platform}] #{res.failures} test(s) failed:".red
+        for f in res.failed
+          console.log "-   #{f.fullTitle}".red
+          console.log "    #{f.error.message}".red
+      else
+        console.log "[#{browser.browserName} #{browser.version} #{browser.platform}] All tests passed".green
+    
+    # Start tests
+    cloud.start (err, res) ->
+      console.log(err) if err?
+      done(!err? && res[0].failures == 0)
+
+  # Release meta-task
+  grunt.registerTask "release", ["jshint", "concat", "uglify", "docco", "git-tag", "s3"]
+
+  # Run a webserver for testing
+  grunt.registerTask "server", ["connect:server:keepalive"]
+
+  # Run tests
+  grunt.registerTask "test", ["connect", "mocha-cloud"]
+
+  # Default meta-task
+  grunt.registerTask "default", ["jshint", "concat", "uglify", "docco"]
---
+++ test/test.bugsnag.js
@@ -0,0 +1,244 @@
+describe("Bugsnag", function () {
+  beforeEach(buildUp);
+  afterEach(tearDown);
+
+  describe("notifyException", function () {
+    it("should not notify if apiKey is not set", function () {
+      Bugsnag.apiKey = null;
+      Bugsnag.notifyException(new Error("Example error"));
+
+      assert(!Bugsnag.testRequest.called, "Bugsnag.testRequest should not have been called");
+    });
+
+    it("should not notify if apiKey is invalid", function () {
+      Bugsnag.apiKey = "bad-api-key";
+      Bugsnag.notifyException(new Error("Example error"));
+
+      assert(!Bugsnag.testRequest.called, "Bugsnag.testRequest should not have been called");
+    });
+
+    it("should contain an apiKey", function () {
+      Bugsnag.notifyException(new Error("Example error"));
+
+      assert(Bugsnag.testRequest.calledOnce, "Bugsnag.testRequest should have been called once");
+      assert(requestData().params.apiKey, "apiKey should be in request params");
+    });
+
+    it("should contain exception class name", function () {
+      Bugsnag.notifyException(new URIError("Example error"));
+
+      assert(Bugsnag.testRequest.calledOnce, "Bugsnag.testRequest should have been called once");
+      assert.equal(requestData().params.name, "URIError");
+    });
+
+    it("should contain the custom class name if overridden", function () {
+      Bugsnag.notifyException(new URIError("Example error"), "CustomError");
+
+      assert(Bugsnag.testRequest.calledOnce, "Bugsnag.testRequest should have been called once");
+      assert.equal(requestData().params.name, "CustomError");
+    });
+
+    it("should contain the correct exception message", function () {
+      Bugsnag.notifyException(new Error("Example error"));
+
+      assert(Bugsnag.testRequest.calledOnce, "Bugsnag.testRequest should have been called once");
+      assert.equal(requestData().params.message, "Example error");
+    });
+
+    it("should contain a stacktrace", function () {
+      try {
+        throw new Error("Example error");
+      } catch (e) {
+        Bugsnag.notifyException(e);
+      }
+
+      assert(Bugsnag.testRequest.calledOnce, "Bugsnag.testRequest should have been called once");
+      assert(requestData().params.stacktrace != null, "stacktrace should be in request params");
+    });
+
+    it("should not notify if releaseStage isn't in notifyReleaseStages", function () {
+      Bugsnag.notifyReleaseStages = ["production"];
+      Bugsnag.releaseStage = "development";
+      Bugsnag.notifyException(new Error("Example error"));
+
+      assert(!Bugsnag.testRequest.called, "Bugsnag.testRequest should not have been called");
+    });
+
+    it("should notify if a custom releaseStage is in notifyReleaseStages", function () {
+      Bugsnag.notifyReleaseStages = ["production", "custom"];
+      Bugsnag.releaseStage = "custom";
+      Bugsnag.notifyException(new Error("Example error"));
+
+      assert(Bugsnag.testRequest.calledOnce, "Bugsnag.testRequest should have been called once");
+    });
+
+    it("should contain global metaData if set", function () {
+      var metaData = {some: {data: "here"}};
+
+      Bugsnag.metaData = metaData;
+      Bugsnag.notifyException(new Error("Example error"));
+
+      assert(Bugsnag.testRequest.calledOnce, "Bugsnag.testRequest should have been called once");
+      assert.deepEqual(requestData().params.metaData, metaData, "metaData should match");
+    });
+
+    it("should contain local metaData if set", function () {
+      var metaData = {some: {data: "here"}};
+
+      Bugsnag.notifyException(new Error("Example error"), metaData);
+
+      assert(Bugsnag.testRequest.calledOnce, "Bugsnag.testRequest should have been called once");
+      assert.deepEqual(requestData().params.metaData, metaData, "metaData should match");
+    });
+
+    it("should accept local metaData as a third parameter", function () {
+      var metaData = {some: {data: "here"}};
+
+      Bugsnag.notifyException(new Error("Example error"), "CustomError", metaData);
+
+      assert(Bugsnag.testRequest.calledOnce, "Bugsnag.testRequest should have been called once");
+      assert.deepEqual(requestData().params.metaData, metaData, "metaData should match");
+    });
+
+    it("should contain merged metaData if both local and global metaData are set", function () {
+      var globalMetaData = {some: {data: "here"}};
+      var localMetaData = {some: {extra: {data: "here"}}};
+
+      Bugsnag.metaData = globalMetaData;
+      Bugsnag.notifyException(new Error("Example error"), localMetaData);
+
+      assert(Bugsnag.testRequest.calledOnce, "Bugsnag.testRequest should have been called once");
+      assert.deepEqual(requestData().params.metaData, {
+        some: {
+          data: "here",
+          extra: { data: "here" }
+        }
+      }, "metaData should match");
+    });
+
+    it("should use the https://notify.bugsnag.com/js endpoint by default", function () {
+      Bugsnag.notifyException(new Error("Example error"));
+
+      assert(Bugsnag.testRequest.calledOnce, "Bugsnag.testRequest should have been called once");
+      assert.equal(requestData().url, "https://notify.bugsnag.com/js");
+    });
+  });
+
+  describe("notify", function () {
+    it("should contain the correct error name", function () {
+      Bugsnag.notify("CustomError", "Something broke");
+
+      assert(Bugsnag.testRequest.calledOnce, "Bugsnag.testRequest should have been called once");
+      assert.equal(requestData().params.name, "CustomError");
+    });
+
+    it("should contain the correct error message", function () {
+      Bugsnag.notify("CustomError", "Something broke");
+
+      assert(Bugsnag.testRequest.calledOnce, "Bugsnag.testRequest should have been called once");
+      assert.equal(requestData().params.message, "Something broke");
+    });
+
+    it("should contain an auto-generated stacktrace", function () {
+      Bugsnag.notify("CustomError", "Something broke");
+
+      assert(Bugsnag.testRequest.calledOnce, "Bugsnag.testRequest should have been called once");
+      assert(requestData().params.stacktrace != null, "stacktrace should be present");
+    });
+  });
+});
+
+describe("window", function () {
+  beforeEach(buildUp);
+  afterEach(tearDown);
+
+  it("should contain a Bugsnag object", function () {
+    assert("Bugsnag" in window, "should have Bugsnag object on window");
+  });
+
+  describe("onerror", function() {
+    it("should notify bugsnag", function () {
+      Bugsnag._onerror = null; // Disable mocha's onerror for this test
+
+      window.onerror("Something broke", "http://example.com/example.js", 123);
+
+      var params = requestData().params;
+      assert(Bugsnag.testRequest.calledOnce, "Bugsnag.testRequest should have been called once");
+      assert.equal(params.name, "window.onerror");
+      assert.equal(params.message, "Something broke");
+      assert.equal(params.lineNumber, 123);
+    });
+
+    it("should call the original onerror", function () {
+      stub(Bugsnag, "_onerror");
+
+      window.onerror("Something broke", "http://example.com/example.js", 123);
+
+      assert(Bugsnag.testRequest.calledOnce, "Bugsnag.testRequest should have been called once");
+      assert(Bugsnag._onerror.calledOnce, "Bugsnag._onerror should have been called once");
+    });
+  });
+});
+
+function buildUp(cb) {
+  // Keep track of mocha's window.onerror
+  window._onerror = window.onerror;
+
+  // Create bugsnag.js script tag
+  var bugsnag = document.createElement("script");
+  bugsnag.id = "bugsnag";
+  bugsnag.type = "text/javascript";
+  bugsnag.src = "../dist/bugsnag.js";
+  bugsnag.onload = bugsnag.onreadystatechange = function () {
+    if(!this.readyState || this.readyState === "loaded" || this.readyState === "complete") {
+      // Set api key to use when testing
+      Bugsnag.apiKey = "9e68f5104323042c09d8809674e8d05c";
+
+      // Stub out requests
+      stub(Bugsnag, "testRequest");
+
+      // Setup is done
+      cb();
+    }
+  };
+
+  // Add bugsnag.js script tag to dom
+  document.getElementsByTagName("body")[0].appendChild(bugsnag);
+}
+
+function tearDown() {
+  // Remove the bugsnag.js script tag
+  var bugsnag = document.getElementById("bugsnag");
+  bugsnag.parentNode.removeChild(bugsnag);
+
+  // Remove the Bugsnag object
+  window.Bugsnag = null;
+
+  // Reset mocha's window.onerror
+  window.onerror = window._onerror;
+  window._onerror = null
+}
+
+function requestData() {
+  return {
+    url: Bugsnag.testRequest.args[0][0],
+    params: Bugsnag.testRequest.args[0][1]
+  };
+}
+
+// Micro stubbing library
+function stub(obj, fname) {
+  origFunction = obj[fname];
+  obj[fname] = function () {
+    var self = obj[fname];
+    self.args = self.args || [];
+    self.args.push(arguments);
+    self.calledCount = self.calledCount ? self.calledCount + 1 : 1;
+    self.calledOnce = (self.calledCount == 1);
+    self.called = true;
+    self.restore = function () { obj[fname] = origFunction };
+    return self;
+  };
+
+  obj[fname].origFunction = origFunction;
+}
